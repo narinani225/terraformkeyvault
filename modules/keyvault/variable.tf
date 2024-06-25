206a3add8abc659e114ff5a05aecd32947dc3872
narinani225 <100519638+narinani225@users.noreply.github.com>
2024-06-25 19:25:54 +0530

    Update variable.tf

---
+++ modules/keyvault/variable.tf
@@ -0,0 +1,30 @@
+variable "resourcegroupname" {
+  description = "The name of the resource group where resources will be deployed."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where the resources will be deployed."
+  type        = string
+}
+
+variable "keyvaultname" {
+  description = "The name of the Azure Key Vault to create."
+  type        = string
+}
+
+variable "tenantid" {
+  description = "The Azure Active Directory tenant ID."
+  type        = string
+}
+
+variable "objectid" {
+  description = "The object ID of the Azure AD principal to grant access."
+  type        = string
+}
+
+variable "skuname" {
+  description = "The name of the key vault secret to create."
+  type        = string
+}
+
